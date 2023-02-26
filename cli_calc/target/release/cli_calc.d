23a66a7331d22636ba9579bb0da54f6756503c8a
greencrimea <tom.horton100@gmail.com>
2023-02-27 00:37:56 +1100

    .

---
+++ cli_calc/target/release/cli_calc.d
@@ -0,0 +1,1 @@
+/home/gc/VS-Projects/Learning_Rust/cli_calc/target/release/cli_calc: /home/gc/VS-Projects/Learning_Rust/cli_calc/src/main.rs
---
+++ cli_calc/target/release/deps/cli_calc-bc96f278a57723d9.d
@@ -0,0 +1,5 @@
+/home/gc/VS-Projects/Learning_Rust/cli_calc/target/release/deps/cli_calc-bc96f278a57723d9: src/main.rs
+
+/home/gc/VS-Projects/Learning_Rust/cli_calc/target/release/deps/cli_calc-bc96f278a57723d9.d: src/main.rs
+
+src/main.rs:
