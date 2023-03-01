e5efc3aa4a514d897b6eb68795bed720fc4a7405
greencrimea <tom.horton100@gmail.com>
2023-03-01 17:22:10 +1100

    .

--- kvstore/src/main.rs
+++ kvstore/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::{self, OpenOptions, File}, collections::HashMap, path::Path, io::Write};
+use std::{fs::{self, File}, collections::HashMap, path::Path};
 
 fn main() {
     let file_path = Path::new("kv.db");
@@ -12,39 +12,25 @@ fn main() {
 
         let mut database = Database::new(file_path).expect("creating db failed");
         database.map.insert(key, value);
-        fs::remove_file(file_path).expect("failed to remove old file");
-        create_file(file_path);
+        let mut contents = String::new();
 
         for (key, value) in database.map {
+
             println!("{} : {}", key, value);
-            write_keys_values(file_path, key, value).expect("failed to write to file");
+            contents.push_str(&format!("{}\t{}\n", key, value));
         }
+        fs::write(file_path, contents).unwrap();
 
     } else {
 
         println!("{} : {}", key, value);
-        create_file(file_path);
-        write_keys_values(file_path, key, value).expect("failed to write to file");
+        File::create(file_path).expect("error creating file");
+        fs::write(file_path, format!("{}\t{}\n", key, value)).unwrap();
     }
 }
 
 
-
-fn create_file(file_path: &Path) -> () {
-    File::create(file_path).expect("error creating file");
-}
-
-fn write_keys_values(file_path: &Path, key: String, value: String) -> Result<(), std::io::Error> {
-    let mut fileref = OpenOptions::new().append(true).open(file_path).expect("unable to open file");
-    fileref.write_all(combine_keys_values(key, value).as_bytes())   
-}
-
-fn combine_keys_values(key: String, value: String) -> String {
-    format!("{}\t{}\n", key, value)
-}
-
-
-
+  
 struct Database {
     map: HashMap<String, String>,
 }
@@ -60,6 +46,6 @@ impl Database {
             let (key, value) = line.split_once('\t').expect("corrupt database");
             map.insert(key.to_owned(), value.to_owned());
         }
-        Ok(Database { map: map })
+        Ok(Database { map })
     }
 }
